5946c97ad59d4bcf64a526c7739ff85f8f2154a1
kukurydza111 <hanka.piskorska@gmail.com>
2026-05-02 15:26:13 +0200

    mapa

---
+++ assets/maps/4760/bruk.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.1-99-gec89c545" name="bruk" tilewidth="32" tileheight="32" spacing="1" margin="1" tilecount="15" columns="5">
+ <image source="../../../Downloads/bruk.png" width="166" height="100"/>
+</tileset>
