99eec06c7a341de3e512b86d79d37cbe63afe814
kukurydza111 <hanka.piskorska@gmail.com>
2026-05-02 11:34:22 +0200

    pp2

--- main.lua
+++ main.lua
@@ -40,7 +40,7 @@ function love.load()
     loadSprite("Marceli")
     sprites["Marceli"].state = "idle"
     sprites["Marceli"].frame = 1
-    animSpeed = 11
+    animSpeed = 10
     animCount = animSpeed
 end
 
