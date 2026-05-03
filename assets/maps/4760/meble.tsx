9adcb9b55875c427318fee97d5d0c771d45530a7
kukurydza111 <hanka.piskorska@gmail.com>
2026-05-03 19:33:21 +0200

    mapa

---
+++ assets/maps/4760/meble.tsx
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1-8-g449813058" name="meble" tilewidth="32" tileheight="32" tilecount="2" columns="2">
+ <image source="../../../../Downloads/meble.png" width="64" height="32"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="3">
+   <object id="5" x="6.03413" y="28.1084" width="19.8592" height="2.97888"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="10.0823" y="27.1154" width="11.8391" height="1.83315"/>
+  </objectgroup>
+ </tile>
+</tileset>
